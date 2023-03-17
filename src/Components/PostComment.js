dd0254770f79bd3d22a90af6842c3d02a410f245
LewPearce <lew@DESKTOP-0NCRVJR>
2023-03-17 11:04:26 +0000

    completed post-comment

--- src/Components/PostComment.js
+++ src/Components/PostComment.js
@@ -3,32 +3,46 @@ import { postComment } from "../axios/api";
 
 export const PostComment = ({ review_id, user, setComments }) => {
   const [newComment, setNewComment] = useState("");
-  console.log(user);
+  const [posting, setPosting] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setPosting(true);
     postComment(review_id, {
       body: newComment,
       username: user.username,
     }).then(({ new_comment }) => {
       setComments((currComments) => [new_comment, ...currComments]);
       setNewComment("");
+      setPosting(false);
     });
   };
 
-  // DISABLE COMMENT BOX IF NOT LOGGED IN
-
+  if (posting) {
+    return;
+  }
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <textarea
-          className="comment__input"
-          onChange={(event) => {
-            setNewComment(event.target.value);
-          }}
-          value={newComment}
-        ></textarea>
-        <button>Submit comment</button>
+        {posting ? (
+          <h2 className="posting">posting your comment...</h2>
+        ) : (
+          <textarea
+            className="comment__input"
+            onChange={(event) => {
+              setNewComment(event.target.value);
+            }}
+            value={newComment}
+            disabled={user === "" ? true : false}
+          ></textarea>
+        )}
+
+        <button
+          className="single__commentButton"
+          disabled={user === "" ? true : false}
+        >
+          {user ? "Submit Comment" : "Sign in to post a comment"}
+        </button>
       </form>
     </>
   );
